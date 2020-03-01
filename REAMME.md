a7e58534be6eff370669438414c15c557599b8dd
Vinit Patil <vinit.patil@mastek.com>
2020-03-01 11:06:13 +0530

    first commit

---
+++ REAMME.md
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch?v=kN1Czs0m1SU
---
+++ main.js
@@ -0,0 +1,22 @@
+const electron  = require('electron');
+const url = require('url');
+const path = require('path');
+
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+//Listen for the app to ready
+
+app.on('ready',function(){
+    // create new window
+    mainWindow = new BrowserWindow({});
+
+    //load html file in window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname,'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true,
+    }))
+})
